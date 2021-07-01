14b46d5ac9197ab2d9721b63b4dad33170ee8ebd
Robotechnic <45141234+Robotechnic@users.noreply.github.com>
2021-07-01 20:42:02 +0200

    Delete dist directory

--- dist/js/chunk-aa80082c.7dfa22e5.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-aa80082c"],{"3c0f":function(c,e,t){},"8cdb":function(c,e,t){"use strict";t.r(e);var n=t("7a23"),s=Object(n["h"])("h1",{class:"p404"},"404",-1),a=Object(n["h"])("p",{class:"p404Message"},"The page that you search doesn't exist yet ;(",-1);function u(c,e){return Object(n["q"])(),Object(n["e"])(n["a"],null,[s,a],64)}t("8d80");const o={};o.render=u;e["default"]=o},"8d80":function(c,e,t){"use strict";t("3c0f")}}]);
-//# sourceMappingURL=chunk-aa80082c.7dfa22e5.js.map
